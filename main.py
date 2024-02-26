dc2127dfa505ec2503448f23515b0412e296ba04
xStFtx <noskillz.exe@gmail.com>
2024-02-26 11:35:42 +0200

    16/0

--- main.py
+++ main.py
@@ -1,49 +1,21 @@
-# from typing import List
-from collections import defaultdict
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
-    def canTraverseAllPairs(self, nums: List[int]) -> bool:
-        def prime_factors(num):
-            factors = set()
-            while num % 2 == 0:
-                factors.add(2)
-                num //= 2
-            for i in range(3, int(num**0.5) + 1, 2):
-                while num % i == 0:
-                    factors.add(i)
-                    num //= i
-            if num > 1:
-                factors.add(num)
-            return factors
-
-        def find(parents, x):
-            if parents[x] == x:
-                return x
-            parents[x] = find(parents, parents[x])
-            return parents[x]
-
-        def union(parents, x, y):
-            root_x = find(parents, x)
-            root_y = find(parents, y)
-            if root_x != root_y:
-                parents[root_x] = root_y
-
-        n = len(nums)
-        parents = [i for i in range(n)]
-
-        prime_factors_dict = defaultdict(list)
-        for i, num in enumerate(nums):
-            factors = prime_factors(num)
-            for factor in factors:
-                prime_factors_dict[factor].append(i)
-
-        for factor_indices in prime_factors_dict.values():
-            for i in range(len(factor_indices) - 1):
-                union(parents, factor_indices[i], factor_indices[i + 1])
-
-        root = find(parents, 0)
-        for i in range(1, n):
-            if find(parents, i) != root:
-                return False
-
-        return True
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # If both trees are empty, they are the same
+        if not p and not q:
+            return True
+        # If one of the trees is empty, they are not the same
+        if not p or not q:
+            return False
+        # If the values of the current nodes are different, they are not the same
+        if p.val != q.val:
+            return False
+        
+        # Recursively check the left and right subtrees
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
