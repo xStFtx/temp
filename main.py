0c448a40f5f2a586ff5c294383d4b259bf2ce4b2
xStFtx <noskillz.exe@gmail.com>
2024-03-01 08:30:29 +0200

    21.0

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
-from collections import deque
-from typing import Optional
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
-    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        # Find the last occurrence of '1'
+        last_one_index = s.rfind('1')
+        
+        # If there is only one '1', return the input string as is
+        if last_one_index == 0:
+            return s
+        
+        # If there are multiple '1's, move the last '1' to the end
+        # and remove the original occurrence of '1'
+        modified_s = s[:last_one_index] + s[last_one_index+1:] + '1'
+        
+        return modified_s
 
-        queue = deque([root])
-        level = 0  # Level index
-
-        while queue:
-            size = len(queue)
-            prev_value = float('-inf') if level % 2 == 0 else float('inf')
-
-            for _ in range(size):
-                node = queue.popleft()
-
-                # Check the value at the current level
-                if (level % 2 == 0 and (node.val % 2 == 0 or node.val <= prev_value)) or \
-                   (level % 2 == 1 and (node.val % 2 == 1 or node.val >= prev_value)):
-                    return False
-
-                prev_value = node.val
-
-                # Add children to the queue
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-            level += 1
-
-        return True
