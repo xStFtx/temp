c5481d0cfcfaee5aee47dafe81c1c53f228bc133
xStFtx <noskillz.exe@gmail.com>
2024-03-02 08:55:17 +0200

    21.0

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-class Solution:
-    def maximumOddBinaryNumber(self, s: str) -> str:
-        # Find the last occurrence of '1'
-        last_one_index = s.rfind('1')
-        
-        # If there is only one '1', return the input string as is
-        if last_one_index == 0:
-            return s
-        
-        # If there are multiple '1's, move the last '1' to the end
-        # and remove the original occurrence of '1'
-        modified_s = s[:last_one_index] + s[last_one_index+1:] + '1'
-        
-        return modified_s
+from typing import List
 
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        # Square each element in the array
+        squares = [num ** 2 for num in nums]
+        
+        # Sort the squared array
+        squares.sort()
+        
+        return squares
