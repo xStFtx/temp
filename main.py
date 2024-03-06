4507dff0ebf6987eb9b1f4c072e90afc440e9098
xStFtx <noskillz.exe@gmail.com>
2024-03-06 07:33:37 +0200

    25.0

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 class Solution:
-    def minimumLength(self, s: str) -> int:
-        # Initialize two pointers
-        left, right = 0, len(s) - 1
-
-        # Move the pointers towards each other
-        while left < right and s[left] == s[right]:
-            char = s[left]
-
-            # Move left pointer to the right until a different character is found
-            while left <= right and s[left] == char:
-                left += 1
-
-            # Move right pointer to the left until a different character is found
-            while left <= right and s[right] == char:
-                right -= 1
-
-        # Return the remaining length of the string
-        return max(0, right - left + 1)
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+        
+        slow = head
+        fast = head.next
+        
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        
+        return True
