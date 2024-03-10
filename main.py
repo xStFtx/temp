dc0ad7250894ed3e008f86617457b3c11ebcd835
xStFtx <noskillz.exe@gmail.com>
2024-03-10 10:54:14 +0200

    30.0

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 from typing import List
 
 class Solution:
-    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        # Initialize pointers for both arrays
-        i, j = 0, 0
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # Find the intersection using set operations
+        intersection_set = set(nums1) & set(nums2)
         
-        # Iterate until one of the arrays is exhausted
-        while i < len(nums1) and j < len(nums2):
-            # If the current elements are equal, return the common element
-            if nums1[i] == nums2[j]:
-                return nums1[i]
-            
-            # Move the pointer for the smaller element
-            elif nums1[i] < nums2[j]:
-                i += 1
-            else:
-                j += 1
+        # Convert the set back to a list
+        intersection_list = list(intersection_set)
         
-        # No common element found
-        return -1
+        return intersection_list
