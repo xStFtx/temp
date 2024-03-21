49d4b4e88711eb9f7db697f1e7dc8fe26b5997e4
xStFtx <noskillz.exe@gmail.com>
2024-03-21 08:10:53 +0200

    41.0

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 class Solution:
-    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        # Find the node before the ath node
-        prev_a = None
-        current = list1
-        for _ in range(a):
-            prev_a = current
-            current = current.next
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev_node = None
+        current_node = head
         
-        # Find the node after the bth node
-        prev_b = None
-        for _ in range(b+1):
-            prev_b = current
-            current = current.next
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
         
-        # Connect the nodes
-        prev_a.next = list2
-        while list2.next:
-            list2 = list2.next
-        list2.next = prev_b
-        
-        return list1
+        return prev_node
