dccd3f34565411dc2c9e9dae550336ad8859ab66
xStFtx <noskillz.exe@gmail.com>
2024-03-23 10:47:45 +0200

    44.0

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
-        # Helper function to reverse a linked list
-        def reverseLinkedList(node):
-            prev = None
-            current = node
-            while current:
-                next_node = current.next
-                current.next = prev
-                prev = current
-                current = next_node
-            return prev
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return
         
+        # Step 1: Find the middle of the linked list
         slow = fast = head
-        while fast and fast.next:
+        while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
         
-        # Reverse the second half of the linked list
-        second_half = reverseLinkedList(slow)
+        # Split the list into two halves
+        second_half = slow.next
+        slow.next = None
         
-        # Compare the first and second halves
+        # Step 2: Reverse the second half of the linked list
+        prev = None
+        current = second_half
+        while current:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+        second_half = prev
+        
+        # Step 3: Merge the first half and the reversed second half alternately
+        first_half = head
         while second_half:
-            if head.val != second_half.val:
-                return False
-            head = head.next
-            second_half = second_half.next
-        return True
+            temp1 = first_half.next
+            temp2 = second_half.next
+            first_half.next = second_half
+            second_half.next = temp1
+            first_half = temp1
+            second_half = temp2
