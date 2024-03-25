116c4b805e16d5d32a9959f0cef8f50119c386c4
xStFtx <noskillz.exe@gmail.com>
2024-03-25 09:45:06 +0200

    45.0

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        # Phase 1: Find the intersection point of the two pointers
-        tortoise = hare = nums[0]
-        while True:
-            tortoise = nums[tortoise]
-            hare = nums[nums[hare]]
-            if tortoise == hare:
-                break
-        
-        # Phase 2: Find the entrance to the cycle
-        tortoise = nums[0]
-        while tortoise != hare:
-            tortoise = nums[tortoise]
-            hare = nums[hare]
-        
-        return hare
+    def findDuplicates(self, nums):
+        duplicates = []
+        for num in nums:
+            index = abs(num) - 1
+            if nums[index] < 0:
+                duplicates.append(abs(num))
+            else:
+                nums[index] = -nums[index]
+        return duplicates
