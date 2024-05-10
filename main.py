e8523dbdd4191f6f41a6275b1683bbaa1078eb21
xStFtx <noskillz.exe@gmail.com>
2024-05-10 03:51:43 +0200

    87.0

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from heapq import heappush, heappop
+import heapq
 
 class Solution:
-    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        n = len(happiness)
-        happiness.sort(reverse=True)  # Sort the happiness values in descending order
-        max_heap = [-val for val in happiness]  # Initialize a max-heap with negated values
-        heapq.heapify(max_heap)
-
-        total_happiness = 0
-        for _ in range(k):
-            current_max = -heappop(max_heap)  # Get the maximum remaining happiness value
-            total_happiness += current_max
-            if max_heap:
-                heappush(max_heap, current_max - 1)  # Decrement the happiness values of the remaining children
-
-        return total_happiness
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        n = len(arr)
+        pq = [(-arr[0] / arr[n - 1], 0, n - 1)]
+        
+        for _ in range(k - 1):
+            neg_frac, i, j = heapq.heappop(pq)
+            if i + 1 < j:
+                heapq.heappush(pq, (-arr[i + 1] / arr[j], i + 1, j))
+        
+        neg_frac, i, j = heapq.heappop(pq)
+        return [arr[i], arr[j]]
