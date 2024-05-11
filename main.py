e68c68c66622e7dcdc5186ec303c4b50eaad83e9
root <root@yeet>
2024-05-11 03:23:43 +0200

    91.0

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import heapq
 
 class Solution:
-    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        n = len(arr)
-        pq = [(-arr[0] / arr[n - 1], 0, n - 1)]
-        
-        for _ in range(k - 1):
-            neg_frac, i, j = heapq.heappop(pq)
-            if i + 1 < j:
-                heapq.heappush(pq, (-arr[i + 1] / arr[j], i + 1, j))
-        
-        neg_frac, i, j = heapq.heappop(pq)
-        return [arr[i], arr[j]]
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        workers = sorted([(wage[i] / quality[i], quality[i]) for i in range(len(quality))])
+        heap = [-q for q in workers[:k][1]]
+        heapq.heapify(heap)
+        total_quality = sum([-q for q in heap])
+        min_cost = float('inf')
+
+        for ratio, q in workers:
+            total_quality += q
+            heapq.heappush(heap, -q)
+            if len(heap) > k:
+                total_quality += heapq.heappop(heap)
+            min_cost = min(min_cost, total_quality * ratio)
+
+        return min_cost
