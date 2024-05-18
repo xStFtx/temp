f5254313e649a9fa9a68c0344784b68bf27e6343
xStFtx <noskillz.exe@gmail.com>
2024-05-18 02:22:25 +0200

    99.0

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        if not root:
-            return None
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        self.moves = 0  # Track the total number of moves
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            left_balance = dfs(node.left)
+            right_balance = dfs(node.right)
+
+            # Calculate the balance factor of the current node
+            balance = node.val - 1 + left_balance + right_balance
+
+            # Update total moves with the absolute balance of this node
+            self.moves += abs(balance) 
+
+            # Return the balance of this node for its parent to use
+            return balance
+
+        dfs(root)
         
-        root.left = self.removeLeafNodes(root.left, target)
-        root.right = self.removeLeafNodes(root.right, target)
-        
-        if not root.left and not root.right and root.val == target:
-            return None
-        
-        return root
