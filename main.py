052fc8402dc463eed5a413ea6c39eddde2377e5b
xStFtx <noskillz.exe@gmail.com>
2024-05-22 07:12:14 +0200

    103.0

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(start, current_subset):
-            # Append a copy of the current subset to the results
-            result.append(current_subset[:])
-            # Iterate over the possible next elements
-            for i in range(start, len(nums)):
-                # Include nums[i] in the current subset
-                current_subset.append(nums[i])
-                # Recurse with the next element
-                backtrack(i + 1, current_subset)
-                # Exclude nums[i] from the current subset
-                current_subset.pop()
+    def partition(self, s: str) -> List[List[str]]:
+        def is_palindrome(sub: str) -> bool:
+            return sub == sub[::-1]
+        
+        def backtrack(start: int, path: List[str]):
+            if start == len(s):
+                result.append(path[:])
+                return
+            
+            for end in range(start + 1, len(s) + 1):
+                substring = s[start:end]
+                if is_palindrome(substring):
+                    path.append(substring)
+                    backtrack(end, path)
+                    path.pop()
         
         result = []
         backtrack(0, [])
