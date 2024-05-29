ab15fcd7adc5b2b7e95c0413eb8c1c10b62cbd9a
xStFtx <noskillz.exe@gmail.com>
2024-05-29 11:56:51 +0200

    110.0

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 class Solution:
-    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        start = 0
-        current_cost = 0
-        max_length = 0
+    def numSteps(self, s: str) -> int:
+        # Convert the binary string to an integer
+        num = int(s, 2)
+        steps = 0
         
-        for end in range(len(s)):
-            current_cost += abs(ord(s[end]) - ord(t[end]))
-            
-            while current_cost > maxCost:
-                current_cost -= abs(ord(s[start]) - ord(t[start]))
-                start += 1
-            
-            max_length = max(max_length, end - start + 1)
+        # Perform operations until the number is reduced to 1
+        while num != 1:
+            if num % 2 == 0:
+                # If the number is even, divide by 2
+                num //= 2
+            else:
+                # If the number is odd, add 1
+                num += 1
+            steps += 1
         
-        return max_length
+        return steps
