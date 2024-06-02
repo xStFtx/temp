649024f55c6ab9a08f4d1370c92e1c55155b0f6f
xStFtx <noskillz.exe@gmail.com>
2024-06-02 02:21:03 +0200

    114.0

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 class Solution:
-    def scoreOfString(self, s: str) -> int:
-        score = 0
-        for i in range(len(s) - 1):
-            score += abs(ord(s[i]) - ord(s[i + 1]))
-        return score
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
