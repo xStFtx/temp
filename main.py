3395d4bd286dada56577fc745029adfd35cbb0cd
xStFtx <noskillz.exe@gmail.com>
2024-06-03 02:03:14 +0200

    115.0

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        left, right = 0, len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
+    def appendCharacters(self, s: str, t: str) -> int:
+        # Initialize pointers for both strings
+        i, j = 0, 0
+        # Length of the strings
+        n, m = len(s), len(t)
+        
+        # Iterate through both strings
+        while i < n and j < m:
+            # If characters match, move the pointer on t
+            if s[i] == t[j]:
+                j += 1
+            # Always move the pointer on s
+            i += 1
+        
+        # The number of characters left in t that weren't matched in s
+        return m - j
