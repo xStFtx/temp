e1bbfbab10ba3da97d47726a139dda17f66b71c3
xStFtx <noskillz.exe@gmail.com>
2024-06-07 16:32:45 +0200

    120.0

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-from collections import Counter
-import heapq
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
 
 class Solution:
-    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        if len(hand) % groupSize != 0:
-            return False
-
-        count = Counter(hand)
-        min_heap = list(count.keys())
-        heapq.heapify(min_heap)
-
-        while min_heap:
-            first = min_heap[0]
-            for i in range(first, first + groupSize):
-                if count[i] == 0:
-                    return False
-                count[i] -= 1
-                if count[i] == 0:
-                    if i != min_heap[0]:
-                        return False
-                    heapq.heappop(min_heap)
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+    
+    def search_root(self, word):
+        node = self.root
+        prefix = ""
+        for char in word:
+            if char not in node.children:
+                break
+            node = node.children[char]
+            prefix += char
+            if node.is_end_of_word:
+                return prefix
+        return word
+    
+    def replaceWords(self, dictionary, sentence):
+        # Build the Trie
+        for root in dictionary:
+            self.insert(root)
         
-        return True
+        # Replace words in the sentence
+        words = sentence.split()
+        replaced_words = [self.search_root(word) for word in words]
+        
+        # Reconstruct the sentence
+        return ' '.join(replaced_words)
+
