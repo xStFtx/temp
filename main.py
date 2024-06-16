eedfd34242f3e0316b3b03e41e4071caac14f7fa
xStFtx <noskillz.exe@gmail.com>
2024-06-16 04:14:52 +0200

    126.0

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-from heapq import heappop, heappush
+from typing import List
 
 class Solution:
-    def findMaximizedCapital(self, k: int, w: int, profits, capital) -> int:
-        # Min heap for capital, max heap for profits
-        min_capital_heap = []
-        max_profit_heap = []
+    def minPatches(self, nums: List[int], n: int) -> int:
+        patches = 0
+        i = 0
+        miss = 1
         
-        # Populate the min heap with projects sorted by their capital requirements
-        for c, p in zip(capital, profits):
-            heappush(min_capital_heap, (c, p))
-        
-        # Iterate k times or until no more projects can be completed
-        for _ in range(k):
-            # Move all projects that can be started with current capital to the max profit heap
-            while min_capital_heap and min_capital_heap[0][0] <= w:
-                c, p = heappop(min_capital_heap)
-                heappush(max_profit_heap, -p)  # use negative to simulate max heap
-            
-            # If no projects can be started, break the loop
-            if not max_profit_heap:
-                break
-            
-            # Select the project with the maximum profit
-            w += -heappop(max_profit_heap)
-        
-        return w
+        while miss <= n:
+            if i < nums.length and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                patches += 1
+                
+        return patches
