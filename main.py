788ab3d392cf99cf361613c5cd4628ca58617ebc
xStFtx <noskillz.exe@gmail.com>
2024-07-01 07:23:46 +0200

    143.0

--- main.py
+++ main.py
@@ -1,62 +1,15 @@
 from typing import List
 
-class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.rank = [1] * size
-    
-    def find(self, u):
-        if self.parent[u] != u:
-            self.parent[u] = self.find(self.parent[u])
-        return self.parent[u]
-    
-    def union(self, u, v):
-        root_u = self.find(u)
-        root_v = self.find(v)
-        
-        if root_u != root_v:
-            if self.rank[root_u] > self.rank[root_v]:
-                self.parent[root_v] = root_u
-            elif self.rank[root_u] < self.rank[root_v]:
-                self.parent[root_u] = root_v
-            else:
-                self.parent[root_v] = root_u
-                self.rank[root_u] += 1
-            return True
-        return False
-
 class Solution:
-    def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
-        # Create two separate Union-Find structures for Alice and Bob
-        uf_alice = UnionFind(n + 1)
-        uf_bob = UnionFind(n + 1)
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        consecutive_odds = 0  # Counter for consecutive odd numbers
         
-        edges_used = 0
+        for num in arr:
+            if num % 2 == 1:
+                consecutive_odds += 1
+                if consecutive_odds == 3:
+                    return True
+            else:
+                consecutive_odds = 0
         
-        # Step 1: Add all type 3 edges (shared by both Alice and Bob)
-        for type_, u, v in edges:
-            if type_ == 3:
-                if uf_alice.union(u, v):
-                    uf_bob.union(u, v)
-                    edges_used += 1
-        
-        # Step 2: Add all type 1 edges (Alice only)
-        for type_, u, v in edges:
-            if type_ == 1:
-                if uf_alice.union(u, v):
-                    edges_used += 1
-        
-        # Step 3: Add all type 2 edges (Bob only)
-        for type_, u, v in edges:
-            if type_ == 2:
-                if uf_bob.union(u, v):
-                    edges_used += 1
-        
-        # Check if both Alice and Bob can fully traverse the graph
-        # They should be able to reach n nodes (considering 1-based indexing)
-        if len(set(uf_alice.find(i) for i in range(1, n + 1))) != 1 or \
-           len(set(uf_bob.find(i) for i in range(1, n + 1))) != 1:
-            return -1
-        
-        # The maximum number of edges we can remove
-        return len(edges) - edges_used
+        return False
