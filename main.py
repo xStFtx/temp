20ea6abdd453aa41494e7c962f5dcf7e25f30482
xStFtx <noskillz.exe@gmail.com>
2024-07-23 18:04:54 +0200

    2.0

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
-from typing import List
-from collections import defaultdict
+from collections import Counter
 
 class Solution:
-    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
-        def topological_sort(conditions):
-            graph = defaultdict(list)
-            in_degree = defaultdict(int)
-            
-            for above, below in conditions:
-                graph[above].append(below)
-                in_degree[below] += 1
-            
-            queue = [i for i in range(1, k + 1) if in_degree[i] == 0]
-            result = []
-            
-            while queue:
-                node = queue.pop(0)
-                result.append(node)
-                
-                for neighbor in graph[node]:
-                    in_degree[neighbor] -= 1
-                    if in_degree[neighbor] == 0:
-                        queue.append(neighbor)
-            
-            return result if len(result) == k else []
-
-        row_order = topological_sort(rowConditions)
-        col_order = topological_sort(colConditions)
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        # Count the frequency of each number
+        freq = Counter(nums)
         
-        if not row_order or not col_order:
-            return []
+        # Create a list of tuples (num, frequency)
+        num_freq = [(num, freq[num]) for num in nums]
         
-        position = {num: (row_order.index(num), col_order.index(num)) for num in range(1, k + 1)}
+        # Sort the list based on frequency (ascending) and value (descending)
+        num_freq.sort(key=lambda x: (x[1], -x[0]))
         
-        matrix = [[0] * k for _ in range(k)]
-        for num, (i, j) in position.items():
-            matrix[i][j] = num
-        
-        return matrix
+        # Return the sorted list of numbers
+        return [num for num, _ in num_freq]
