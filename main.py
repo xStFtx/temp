075c64038a178767439c463af4c6af098b382297
xStFtx <noskillz.exe@gmail.com>
2024-07-25 05:42:49 +0200

    4.0

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 class Solution:
-    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        def get_mapped_value(num: int) -> int:
-            if num == 0:
-                return mapping[0]
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def merge_sort(arr, left, right):
+            if left < right:
+                mid = (left + right) // 2
+                merge_sort(arr, left, mid)
+                merge_sort(arr, mid + 1, right)
+                merge(arr, left, mid, right)
+
+        def merge(arr, left, mid, right):
+            left_half = arr[left:mid+1]
+            right_half = arr[mid+1:right+1]
             
-            mapped = 0
-            multiplier = 1
-            while num > 0:
-                digit = num % 10
-                mapped += mapping[digit] * multiplier
-                multiplier *= 10
-                num //= 10
-            return mapped
-        
-        indexed_nums = [(i, num, get_mapped_value(num)) for i, num in enumerate(nums)]
-        indexed_nums.sort(key=lambda x: (x[2], x[0]))
-        
-        return [num for _, num, _ in indexed_nums]
+            i = j = 0
+            k = left
+            
+            while i < len(left_half) and j < len(right_half):
+                if left_half[i] <= right_half[j]:
+                    arr[k] = left_half[i]
+                    i += 1
+                else:
+                    arr[k] = right_half[j]
+                    j += 1
+                k += 1
+            
+            while i < len(left_half):
+                arr[k] = left_half[i]
+                i += 1
+                k += 1
+            
+            while j < len(right_half):
+                arr[k] = right_half[j]
+                j += 1
+                k += 1
+
+        merge_sort(nums, 0, len(nums) - 1)
+        return nums
